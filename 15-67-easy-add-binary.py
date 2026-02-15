44ba035cbfe071639ae8a7e5ab93b6c771d7f212
Aditya <adityashirsatrao007@gmail.com>
2026-02-15 22:57:25 +0530

    Refactor addBinary method for simplicity

--- 15-67-easy-add-binary.py
+++ 15-67-easy-add-binary.py
@@ -1,22 +1,6 @@
 class Solution:
-    def addBinary(self, a: str, b: str) -> str:
-        maxL = max(len(a), len(b))
-        C = 0
-        res = []
-
-        def sum_bit(A, B, C):
-            return (A ^ B) ^ C
-
-        def carry_bit(A, B, C):
-            return (A & B) | ((A ^ B) & C)
-
-        for i in range(maxL):
-            A = a[-1 - i] & 1 if i < len(a) else 0
-            B = b[-1 - i] & 1 if i < len(b) else 0
-
-            res.append(str(sum_bit(A, B, C)))
-            C = carry_bit(A, B, C)
-
-        if C: res.append(str(C))
-
-        return "".join(reversed(res))
+    def addBinary(self, a, b) -> str:
+        x, y = int(a, 2), int(b, 2)
+        while y:
+            x, y = x ^ y, (x & y) << 1
+        return bin(x)[2:]
